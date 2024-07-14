e81f0cf258eca55a29e04912aad74a7df5745299
Benjamin Mugnier <mugnier.benjamin@gmail.com>
2024-07-14 18:45:51 +0200

    Split SidePanel
    We can't connect to signals from a node in an instantiated scene. Add
    glue to do so.

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,18 +1,13 @@
 extends Node
 
-@export var gold_label : Label
 @export var gold : int = 10
+
+signal gold_changed(gold : int)
 
 
 func _ready() -> void:
-	gold_label.text = str(gold)
+	gold_changed.emit(gold)
 
-
-func _on_end_turn_button_end_turn() -> void:
-	for offerings in get_tree().get_nodes_in_group("offerings"):
-		for offering : CheckBox in offerings.get_children():
-			if offering.button_pressed:
-				gold -= int(offering.text)
-				break
-	assert(gold >= 0, "Gold amount is negative")
-	gold_label.text = str(gold)
+func make_offering(offer : int) -> void:
+	gold -= offer
+	gold_changed.emit(gold)
---
+++ entities/ui/side_panel/side_panel.gd
@@ -0,0 +1,19 @@
+extends PanelContainer
+
+func _on_player_gold_changed(gold: int) -> void:
+	$Commands/HBoxContainer/GoldValue.text = str(gold)
+
+func _on_end_turn_button_button_down() -> void:
+	#Execute offering
+	for offerings in get_tree().get_nodes_in_group("offerings"):
+		for offering : CheckBox in offerings.get_children():
+			if offering.button_pressed:
+				%Player.make_offering(int(offering.text))
+				break;
+	#Refresh UI
+	for offerings in get_tree().get_nodes_in_group("offerings"):
+		for offering : CheckBox in offerings.get_children():
+			offering.disabled = false
+			offering.button_pressed = false
+			if %Player.gold < int(offering.text):
+				offering.disabled = true
