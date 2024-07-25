1d422c5442832deacf69986d3124c36ea370013b
Benjamin Mugnier <mugnier.benjamin@gmail.com>
2024-07-25 18:07:59 +0200

    Support mouse deconfining
    Deconfine with ESCAPE, this is en Input Map project setting.
    Reconfine with left click on the game.

--- entities/camera/board_camera.gd
+++ entities/camera/board_camera.gd
@@ -20,6 +20,18 @@ func move_camera(delta: float) -> void:
 	position.x += move.x * speed * delta
 	position.z += move.y * speed * delta
 
+func handle_mouse_confinement(event: InputEvent) -> void:
+	if event.is_action_pressed("deconfine_mouse"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	if event is InputEventMouseButton:
+		#Re-confine on left mouse click
+		var mouse_button: InputEventMouseButton = event
+		if mouse_button.button_index == MOUSE_BUTTON_LEFT and mouse_button.is_pressed():
+			Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+
 func _process(delta: float) -> void:
 	if Input.get_mouse_mode() == Input.MOUSE_MODE_CONFINED:
 		move_camera(delta)
+
+func _input(event: InputEvent) -> void:
+	handle_mouse_confinement(event)
