9b67b5960a1902893779594fdd05ab0cbafda1b6
Benjamin Mugnier <mugnier.benjamin@gmail.com>
2024-08-20 19:45:45 +0200

    Remove duplicate 'god' folder

--- entities/ui/god/god.gd
+++
@@ -1,8 +0,0 @@
-extends VBoxContainer
-
-@export var god_name : String
-@export_multiline var god_tooltip : String
-
-func _ready() -> void:
-	$Name.text = god_name
-	$Name.tooltip_text = god_tooltip
